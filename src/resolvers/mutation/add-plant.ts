8498db12af7f2f8c6a51677743ef67d284d3e1b6
tamif <tamarafrazzetta@gmail.com>
2023-11-04 19:32:13 -0300

    feat: add validation to addPlant

--- src/resolvers/mutation/add-plant.ts
+++ src/resolvers/mutation/add-plant.ts
@@ -1,5 +1,7 @@
 import { Plant, plants } from '../../datasources/dataset';
 import { v4 as uuid } from 'uuid';
+import { ApolloServerErrorCode } from '@apollo/server/errors';
+import { GraphQLError } from 'graphql';
 
 type AddPlantArgs = {
   name: string;
@@ -8,6 +10,11 @@ type AddPlantArgs = {
 };
 
 export const addPlant = (root: Plant, args: AddPlantArgs) => {
+  if (plants.find((plant) => plant.name === args.name))
+    throw new GraphQLError('Plant already exists', {
+      extensions: { code: ApolloServerErrorCode.BAD_USER_INPUT },
+    });
+
   const plant = { ...args, id: uuid() };
   plants.push(plant);
   return plant;
