edd8523d888646653e56bd2c7b9ac8f7b5053e71
tamif <tamarafrazzetta@gmail.com>
2023-11-04 19:55:07 -0300

    feat: add json-server

--- src/resolvers/query/all-plants.ts
+++ src/resolvers/query/all-plants.ts
@@ -1,10 +1,14 @@
-import { Plant, plants } from '../../datasources/dataset';
+import axios from 'axios';
+import { Plant } from '../../datasources/dataset';
 
 type AllPlantsArgs = {
   price?: 'YES' | 'NO';
 };
 
-export const allPlants = (root: Plant, args: AllPlantsArgs) => {
+export const allPlants = async (root: Plant, args: AllPlantsArgs) => {
+  const { data: plants } = await axios.get<Plant[]>(
+    'http://localhost:3001/plants'
+  );
   if (!args.price) {
     return plants;
   }
